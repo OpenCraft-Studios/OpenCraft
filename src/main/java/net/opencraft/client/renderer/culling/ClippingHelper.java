8f8b5b1a313fef883864b75492187adf97fe8b78
Ciro Diaz <cirod307@gmail.com>
2024-09-21 19:31:25 +0200

    So much nesting

--- src/main/java/net/opencraft/client/renderer/culling/ClippingHelper.java
+++ src/main/java/net/opencraft/client/renderer/culling/ClippingHelper.java
@@ -15,26 +15,24 @@ public class ClippingHelper {
         this.clippingMatrix = new float[16];
     }
 
-    public boolean isBoxInFrustum(final double double1, final double double2, final double double3, final double double4, final double double5, final double double6) {
+    public boolean isBoxInFrustum(double a, double b, double c, double d, double e, double f) {
         for (int i = 0; i < 6; ++i) {
-            if (this.frustum[i][0] * double1 + this.frustum[i][1] * double2 + this.frustum[i][2] * double3 + this.frustum[i][3] <= 0.0) {
-                if (this.frustum[i][0] * double4 + this.frustum[i][1] * double2 + this.frustum[i][2] * double3 + this.frustum[i][3] <= 0.0) {
-                    if (this.frustum[i][0] * double1 + this.frustum[i][1] * double5 + this.frustum[i][2] * double3 + this.frustum[i][3] <= 0.0) {
-                        if (this.frustum[i][0] * double4 + this.frustum[i][1] * double5 + this.frustum[i][2] * double3 + this.frustum[i][3] <= 0.0) {
-                            if (this.frustum[i][0] * double1 + this.frustum[i][1] * double2 + this.frustum[i][2] * double6 + this.frustum[i][3] <= 0.0) {
-                                if (this.frustum[i][0] * double4 + this.frustum[i][1] * double2 + this.frustum[i][2] * double6 + this.frustum[i][3] <= 0.0) {
-                                    if (this.frustum[i][0] * double1 + this.frustum[i][1] * double5 + this.frustum[i][2] * double6 + this.frustum[i][3] <= 0.0) {
-                                        if (this.frustum[i][0] * double4 + this.frustum[i][1] * double5 + this.frustum[i][2] * double6 + this.frustum[i][3] <= 0.0) {
-                                            return false;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+            if (isOutsidePlane(i, a, b, c) &&
+                isOutsidePlane(i, d, b, c) &&
+                isOutsidePlane(i, a, e, c) &&
+                isOutsidePlane(i, d, e, c) &&
+                isOutsidePlane(i, a, b, f) &&
+                isOutsidePlane(i, d, b, f) &&
+                isOutsidePlane(i, a, e, f) &&
+                isOutsidePlane(i, d, e, f)) {
+                return false;
             }
         }
         return true;
     }
+
+    private boolean isOutsidePlane(int i, double x, double y, double z) {
+        return this.frustum[i][0] * x + this.frustum[i][1] * y + this.frustum[i][2] * z + this.frustum[i][3] <= 0.0;
+    }
+
 }
