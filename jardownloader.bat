623c13018231d4cac0c40501003fd00d51b4adfd
Ciro Diaz <cirod307@gmail.com>
2024-09-22 22:03:56 +0200

    🛠️ `jardownloader,bat` wasn't working!

--- jardownloader.bat
+++ jardownloader.bat
@@ -1,4 +1,1 @@
-@ECHO OFF
-:: Use browser
-  set "URL=https://github.com/OpenCraft-Studios/OpenCraft/actions/runs/10983895248/artifacts/1963680486"
-  start "" "%URL%"
+@start "" "https://github.com/OpenCraft-Studios/OpenCraft/actions/runs/10983895248/artifacts/1963680486"
