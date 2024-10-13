8338311d01f5582a0ae253a3b9a7ab3231dd6309
Ciro Diaz <cirod307@gmail.com>
2024-10-13 13:28:08 +0200

    ➕ Add 4 `pickRandom` overload methods

--- src/main/java/net/opencraft/util/Splashes.java
+++ src/main/java/net/opencraft/util/Splashes.java
@@ -2,8 +2,9 @@ package net.opencraft.util;
 
 import java.io.*;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
@@ -28,6 +29,28 @@ public class Splashes {
 	
 	public void load(URI uri) {
 		throw new UnsupportedOperationException();
+	}
+	
+	/* Pick Random overload */
+	public String pickRandom() {
+		return pickRandom(Math::random);
+	}
+	
+	public String pickRandom(Random random) {
+		return pickRandom(size -> random.nextInt(size));
+	}
+	
+	public String pickRandom(Supplier<Double> rnd) {
+		Function<Integer, Integer> func = size -> (int) (rnd.get() * size);
+		return pickRandom(func);
+	}
+	
+	public String pickRandom(Function<Integer, Integer> rnd) {
+		int len = splashes.size();
+		if (len == 1)
+			return splashes.get(0);
+		
+		return splashes.get(rnd.apply(len));
 	}
 	
 }
